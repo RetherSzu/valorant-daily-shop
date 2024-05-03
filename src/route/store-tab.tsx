9deb9588aa67c06bc90ed72152923dc38d1304f8
Bourgain Wilbal Nicolas <nicolas_bourgainwilbal@ens.univ-artois.fr>
2024-05-03 18:04:49 +0200

    :hammer: change bottom tab bar

--- src/route/store-tab.tsx
+++ src/route/store-tab.tsx
@@ -1,20 +1,55 @@
-import React from "react";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import React, { useState } from "react";
+import { BottomNavigation } from "react-native-paper";
+// component
+import SvgShop from "@/component/icon/shop";
+import SvgSetting from "@/component/icon/setting";
+// context
+import useThemeContext from "@/context/hook/use-theme-context";
 // route
+import Header from "@/route/navigation/header";
 import StoreStackScreen from "@/route/store-stack-screen";
-import BottomTabBar from "@/route/navigation/bottom-tab-bar";
 // screen
 import Settings from "@/screen/settings";
-import Header from "@/route/navigation/header";
-
-const Tab = createBottomTabNavigator();
 
 const StoreTab = () => {
+
+    const [index, setIndex] = useState(0);
+
+    const [routes] = React.useState([
+        { key: "shop", focusedIcon: SvgShop },
+        { key: "settings", focusedIcon: SvgSetting },
+    ]);
+
+    const renderScene = BottomNavigation.SceneMap({
+        shop: StoreStackScreen,
+        settings: Settings,
+    });
+
+    const { colors } = useThemeContext();
+
     return (
-        <Tab.Navigator tabBar={(props) => <BottomTabBar {...props} />} screenOptions={{ "header": () => <Header /> }}>
-            <Tab.Screen name="Store" component={StoreStackScreen} />
-            <Tab.Screen options={{ headerShown: false }} name="Settings" component={Settings} />
-        </Tab.Navigator>
+        <>
+            <Header />
+            <BottomNavigation
+                labeled={false}
+                sceneAnimationEnabled
+                inactiveColor="#7A7B7E"
+                onIndexChange={setIndex}
+                renderScene={renderScene}
+                activeColor={colors.primary}
+                sceneAnimationType="shifting"
+                activeIndicatorStyle={{
+                    width: 48,
+                    height: 48,
+                    padding: 8,
+                    borderRadius: 50,
+                    backgroundColor: colors.card,
+                }}
+                navigationState={{ index, routes }}
+                style={{ backgroundColor: "#1B1D21" }}
+                barStyle={{ backgroundColor: "#1B1D21", justifyContent: "center" }}
+            />
+        </>
     );
 };
 
