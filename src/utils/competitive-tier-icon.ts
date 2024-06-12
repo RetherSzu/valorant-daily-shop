482ce3be70fd1c63207f0be7bb217a703d45caf1
Bourgain Wilbal Nicolas <nicolas_bourgainwilbal@ens.univ-artois.fr>
2024-06-12 12:43:03 +0200

    :hammer: add competitive tier icon

---
+++ src/utils/competitive-tier-icon.ts
@@ -0,0 +1,42 @@
+export const getCompetitiveTierIcon = (rank: string) => {
+
+    const rankSplit = rank.toLowerCase().split(" ");
+
+    rank = rankSplit[0];
+    const rankNumber = rankSplit[1];
+
+    switch (rank.toLowerCase()) {
+        case "iron":
+            if (rankNumber === "1") return require(`../../assets/competitive-tiers/iron-1.png`);
+            if (rankNumber === "2") return require(`../../assets/competitive-tiers/iron-2.png`);
+            return require(`../../assets/competitive-tiers/iron-3.png`);
+        case "bronze":
+            if (rankNumber === "1") return require(`../../assets/competitive-tiers/bronze-1.png`);
+            if (rankNumber === "2") return require(`../../assets/competitive-tiers/bronze-2.png`);
+            return require(`../../assets/competitive-tiers/bronze-3.png`);
+        case "silver":
+            if (rankNumber === "1") return require(`../../assets/competitive-tiers/silver-1.png`);
+            if (rankNumber === "2") return require(`../../assets/competitive-tiers/silver-2.png`);
+            return require(`../../assets/competitive-tiers/silver-3.png`);
+        case "gold":
+            if (rankNumber === "1") return require(`../../assets/competitive-tiers/gold-1.png`);
+            if (rankNumber === "2") return require(`../../assets/competitive-tiers/gold-2.png`);
+            return require(`../../assets/competitive-tiers/gold-3.png`);
+        case "platinum":
+            if (rankNumber === "1") return require(`../../assets/competitive-tiers/platinum-1.png`);
+            if (rankNumber === "2") return require(`../../assets/competitive-tiers/platinum-2.png`);
+            return require(`../../assets/competitive-tiers/platinum-3.png`);
+        case "diamond":
+            if (rankNumber === "1") return require(`../../assets/competitive-tiers/diamond-1.png`);
+            if (rankNumber === "2") return require(`../../assets/competitive-tiers/diamond-2.png`);
+            return require(`../../assets/competitive-tiers/diamond-3.png`);
+        case "immortal":
+            if (rankNumber === "1") return require(`../../assets/competitive-tiers/immortal-1.png`);
+            if (rankNumber === "2") return require(`../../assets/competitive-tiers/immortal-2.png`);
+            return require(`../../assets/competitive-tiers/immortal-3.png`);
+        case "radiant":
+            return require(`../../assets/competitive-tiers/radiant.png`);
+        default:
+            return require(`../../assets/competitive-tiers/unranked.png`);
+    }
+};
