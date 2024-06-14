bc0b952cad6881206f4341b3a227e8b338693659
Usermode <scottjsh@whitesky.kr>
2024-06-14 21:39:05 +0900

    🔨 fix ascendant tier icon

--- src/utils/competitive-tier-icon.ts
+++ src/utils/competitive-tier-icon.ts
@@ -30,6 +30,10 @@ export const getCompetitiveTierIcon = (rank: string) => {
             if (rankNumber === "1") return require(`../../assets/competitive-tiers/diamond-1.png`);
             if (rankNumber === "2") return require(`../../assets/competitive-tiers/diamond-2.png`);
             return require(`../../assets/competitive-tiers/diamond-3.png`);
+        case "ascendant":
+            if (rankNumber === "1") return require(`../../assets/competitive-tiers/ascendant-1.png`);
+            if (rankNumber === "2") return require(`../../assets/competitive-tiers/ascendant-2.png`);
+            return require(`../../assets/competitive-tiers/ascendant-3.png`);
         case "immortal":
             if (rankNumber === "1") return require(`../../assets/competitive-tiers/immortal-1.png`);
             if (rankNumber === "2") return require(`../../assets/competitive-tiers/immortal-2.png`);
